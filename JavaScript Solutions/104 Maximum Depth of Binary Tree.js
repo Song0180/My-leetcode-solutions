54173e9069026acb16c3c20701f1f1508c581705
song0180 <song0180@e.ntu.edu.sg>
2021-08-19 12:18:56 +0800

    add other solution for 104

--- JavaScript Solutions/104 Maximum Depth of Binary Tree.js
+++ JavaScript Solutions/104 Maximum Depth of Binary Tree.js
@@ -38,3 +38,11 @@ var maxDepth = function (root) {
   dfs(root);
   return max;
 };
+
+// O(n)
+const maxDepth = root => {
+  if (!root) return 0;
+  let left = maxDepth(root.left);
+  let right = maxDepth(root.right);
+  return Math.max(left, right) + 1;
+};
