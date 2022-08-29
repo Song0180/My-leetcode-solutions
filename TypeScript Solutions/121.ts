3d379bf8daed22a089ae81828237f84aa4305c3a
song0180 <song0180@e.ntu.edu.sg>
2022-08-29 11:29:11 +0800

    add 121

---
+++ TypeScript Solutions/121.ts
@@ -0,0 +1,12 @@
+function maxProfit(prices: number[]): number {
+  let min = Infinity;
+  let max = 0;
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < min) {
+      min = prices[i];
+    } else if (prices[i] - min > max) {
+      max = prices[i] - min;
+    }
+  }
+  return max;
+}
